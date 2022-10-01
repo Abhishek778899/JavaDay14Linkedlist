b6d1db66a2a494064e33e99675c6a03807e758ff
Abhishek778899 <abhisheksuryawanshi325@gmail.com>
2022-10-01 12:21:04 +0530

    Addsimplelist

---
+++ src/main/java/Linkedlist/Link/Addsimplelist.java
@@ -0,0 +1,19 @@
+package Linkedlist.Link;
+
+import java.util.LinkedList;
+
+public class Addsimplelist 
+{public static void main(String[] args) {
+
+    System.out.println("Welcome to the Linked List Program");
+
+    LinkedList<Integer> linkedList = new LinkedList();
+    linkedList.add(70);
+    linkedList.add(30);
+    linkedList.add(56);
+    linkedList.display();
+
+}
+
+}
+
