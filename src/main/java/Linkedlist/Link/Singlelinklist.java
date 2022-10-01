68bd25cc2a34c106b59ba175e5b6a3b93301c61b
Abhishek778899 <abhisheksuryawanshi325@gmail.com>
2022-10-01 12:17:36 +0530

    Added to project file

---
+++ src/main/java/Linkedlist/Link/Singlelinklist.java
@@ -0,0 +1,18 @@
+package Linkedlist.Link;
+
+import java.util.LinkedList;
+
+public class Singlelinklist {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Linked List Program");
+
+		LinkedList<Integer> linkedList = new LinkedList();
+		linkedList.add(70);
+		linkedList.add(30);
+		linkedList.add(56);
+		((Object) linkedList).display();
+
+	}
+
+}
